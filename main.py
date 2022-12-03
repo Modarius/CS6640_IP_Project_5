b6347e68641e574530ab543385977a1293bc246a
Modarius <alancfelt@gmail.com>
2022-12-02 22:09:10 -0700

    update

--- main.py
+++ main.py
@@ -4,46 +4,25 @@ Image Processing Project 1: Adapted from Project 0
 """
 
 
+from copy import deepcopy
 import os
-import string
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
-import matplotlib.colors as colors
+import skimage.io as io
 import skimage.measure as measure
+import skimage.filters as filters
 import skimage.morphology as morph
-import skimage.exposure as exp
-import skimage.util as imageutil
+import skimage.restoration as restore
+import skimage.transform as transform
+import predict as predict
 
-import time
-
-def getHistogram(im, bins):
-    histogram = np.zeros(bins, np.uint32) 
-    if (np.any(im > 1)):
-        im = im / 255  # normalize pixel intensity to [0,1]
-    im = np.int32(np.round(im * (bins - 1)))  # scale pixel intensity to [0, bins-1]
-    flat_im = np.around(im.flatten()) # flatten matrix to 1D array
-    for i in np.arange(bins):
-        histogram[i] = len(np.extract(np.equal(flat_im, i), flat_im)) # get the number of pixels equal to the bin number
-    return histogram
-
-def threshold(im, low, high):
-    if (low > high): # basic error checking 
-        temp = low
-        low = high
-        high = temp
-    # if (np.any(im > 1)): # shouldn't be needed but good to check
-    #     im = im / 255  # normalize pixel intensity to [0,1]
-    # im = np.int32(np.around(im * 255))  # scale pixel intensity to [0, 255] (256 intensity values)
-    # get binary if pixel intensity is out of bounds, then invert it to get whether it is in the threshold range
-    threshold = ~np.array((im < low) | (im > high)) 
-    return threshold
-
-def connectedComponents(im, min_size):
-    out_im = np.zeros_like(im,dtype=int)
-    [labeled_im, num] = measure.label(im, return_num=True, connectivity=2)
+def connectedComponents(image, min_size=5):
+    out_im = np.zeros_like(image,dtype=int)
+    image = morph.remove_small_objects(image, min_size=min_size)
+    image = morph.remove_small_holes(image, area_threshold=min_size)
+    [labeled_im, num] = morph.label(image, return_num=True, connectivity=2)
     if (num > 1): # prevents case where only one label is passed to remove_small_objects() which throws a warning
-        labeled_im = morph.remove_small_objects(labeled_im, min_size=min_size)
         labels = np.unique(labeled_im) # find all the labels in the image
         labels = np.delete(labels, [0]) # delete the background label
         label_num = 1
@@ -55,151 +34,76 @@ def connectedComponents(im, min_size):
         out_im = labeled_im
     return out_im
 
-def histogramEqualization(im):
-    hist, bin_edges = np.histogram(im, 256)
-    p_r = hist/im.size # normalize the histogram to get probability dist. function
-    c_r = np.cumsum(p_r) # get cumulative distribution function (discrete)
-    c_k = np.uint8(np.round(c_r * 255)) # scale the cumulative distribution function to entire intensity range
-    out = np.zeros_like(im)
-
-    # use orig. image values to index into scaled cumulative d. function to get output intensity, requires
-    # reshaping to use the python indexing
-    out = np.reshape(c_k[im.flatten()], im.shape) 
-    return out
+def ocr(image):
+    predictor = predict.CharacterPredictor(model_path="model.pth")
+    denoised_im = restore.denoise_bilateral(image)
+    threshold_im = np.round(restore.denoise_nl_means(denoised_im < filters.threshold_otsu(denoised_im))).astype(bool)
+    labeled_im = connectedComponents(threshold_im)
+    props = ["label", "bbox", "image_intensity"]
+    im_props = pd.DataFrame(measure.regionprops_table(labeled_im, intensity_image=(labeled_im > 0).astype(np.int8), properties=props))
+    mapper = {  "bbox-0": "min_row", "bbox-1": "min_col", "bbox-2": "max_row", "bbox-3": "max_col"}
+    im_props.rename(columns=mapper, inplace=True)
+    im_props.eval("sum_x_y = min_row + min_col", inplace=True)
+    process_props = deepcopy(im_props)
+    chars = []
+    while(process_props.__len__() > 0):
+        chars.append([])
+        idx_tl = process_props['sum_x_y'].idxmin() # index of the top left point
+        tl_props = dict(process_props.loc[idx_tl])
+        current_row = process_props.query("min_row <" + str(tl_props['max_row'])) # get the characters in the current row
+        process_props.drop(index=current_row.index,inplace=True) # remove them from the list of all properties
+        current_row.sort_values(by="min_col", inplace=True) # order left to right
+        i=0
+        figure = plt.figure()
+        for idx in current_row.index:
+            i += 1
+            curr_img = current_row.loc[idx]['image_intensity']
+            scaled_img = transform.rescale(curr_img, 28 / np.array(curr_img.shape).max(), anti_aliasing=False)
+            print(scaled_img.shape)
+            pad_x_begin = 0
+            pad_x_end = 0
+            pad_y_begin = 0
+            pad_y_end = 0
+            if(scaled_img.shape != (28, 28)):
+                row, col = scaled_img.shape
+                pad_x_begin = np.floor((28 - col)/2).astype(np.int8)
+                pad_x_end = np.ceil((28 - col)/2).astype(np.int8)
+                pad_y_begin = np.floor((28-row)/2).astype(np.int8)
+                pad_y_end = np.ceil((28-row)/2).astype(np.int8)
+            padded_img = np.pad(scaled_img, pad_width=((pad_y_begin, pad_y_end),(pad_x_begin, pad_x_end)))
+            char_img = (padded_img > 0).astype(np.int8)
+            ax1 = plt.subplot(2, len(current_row), i)
+            ax2 = plt.subplot(2, len(current_row), i + len(current_row))
+            ax1.imshow(char_img)
+            ax2.imshow(curr_img)
+            plt.setp(figure.axes, xticks=[], yticks=[])
+            chars[-1].append(predictor.predict(char_img))
+        plt.close('all')
+    return
 
 def main():
-    # list of input files
-    # found at https://stackoverflow.com/a/5137509
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    # this directory must contain images only, no folders or other filetypes
-    files = os.listdir(dir_path + "/project-1-images")
-    print(files)  # print out the list of images in the project-1-images folder
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + os.sep # https://stackoverflow.com/a/5137509
+    img_path = "test_images" + os.sep
+    # get example image filepaths
+    example_images = os.listdir(dir_path + img_path)
+    split_text = np.array([temp.split('.') for temp in example_images]) # assumes filenames only have a single '.'
+    img_filenames = list()
+    for i in range(len(split_text)):
+        if split_text[i,-1] in {'jpg', 'png', 'bmp'}:
+            img_filenames.append(split_text[i,-2] + '.' + split_text[i,-1])
+    
+    print(img_filenames)  # print out the list of images in the project-1-images folder
     # find the number of rows and columns needed to display all images in a subplot
     # x = int(np.ceil(np.sqrt(len(files))))
     # y = int(np.ceil(len(files)/x))
 
-    for i in range(len(files)):
-        bins = 256
+    for i in range(len(img_filenames)):
         # read in the image to the variable im
-        im = Image.open(dir_path + "/project-1-images/" + files[i])
-        im = np.asarray(im.convert(mode="L")) # convert image to gray scale (mode="L")
-
-        # Question 1
-        fig, ax = plt.subplots(2,4, figsize=(12,6))
-        plt.subplot(2,4,1)
-        plt.imshow(im, cmap='gray')
-        plt.title("Image: " + files[i])
-        t = time.time()
-        histogram = getHistogram(im, bins)
-        print("Histogram time: " + str(time.time() - t) + "s for " + files[i])# prints out runtime, for debugging/ speed
-        plt.subplot(2,4,5)
-        plt.bar(np.arange(0, 256, 256/bins), histogram)
-        plt.title("Image Histogram")
-
-        # Question 2.1
-        t = time.time()
-        thresh_im = threshold(im, 100, 220) # threshold limits 100 <-> 220
-        print("Threshold time: " + str(time.time() - t) + "s for " + files[i]) # prints out runtime, for debugging/ speed
-        plt.subplot(2,4,2)
-        plt.imshow(thresh_im, cmap='gray')
-        plt.title("Threshold")
-        plt.subplot(2,4,6)
-        thresh_hist, bin_edges = np.histogram(im * thresh_im,bins)
-        plt.bar(np.arange(1, 256, 256/bins), thresh_hist[1:]) # ignore zeros, there are a lot of them
-        plt.title("Thresh. Histogram")
-
-        # Question 2.2
-        t = time.time()
-        cc = connectedComponents(thresh_im, int(thresh_im.size/500)) # minumum size is 1/500th of the number of pixels in the image
-        print("Conn. Comp. time: " + str(time.time() - t) + "s for " + files[i])# prints out runtime, for debugging/ speed
-        plt.subplot(2,4,3)
-        plt.imshow(cc, cmap='tab10')
-        plt.title("Conn. Comps.")
-
-        eq_image = histogramEqualization(im) # call my equalization program
-        plt.subplot(2,4,4)
-        plt.imshow(eq_image, cmap='gray') # show the equalized image
-        plt.title("Equalized")
-        plt.subplot(2,4,8) 
-        eq_hist, bin_edges = np.histogram(eq_image,bins) # get the histogram
-        plt.bar(np.arange(0, 256, 256/bins), eq_hist) # show the histogram of the equalized image
-        plt.title("Equalized Histogram")
-        
-        #fig.subplots_adjust(wspace=0.45)
-        for curr_ax in ax.flatten():
-            curr_ax.set_box_aspect(1.0) # set the plots to be square
-            curr_ax.ticklabel_format(style='sci', axis='y', scilimits=(0,2)) # format x axis to display in scientific notation
-        plt.subplot(2,4,7).remove() # there is no histogram for connected components so don't show the plot there
-        plt.savefig("output" + os.path.sep + files[i].split('.')[0])
-        plt.close('all')
-
-        kern_size = [4, 500, 100, 100] # different options of kernal size to test
-        clip_lim = [.5, .5, .8, .25] # different options of clip limits to test (in conjunction with kern_size)
-        length = len(kern_size)
-        fig, ax = plt.subplots(2,length,figsize=(8,4))
-        for j in np.arange(length):
-            adapt_hist_eq_image = exp.equalize_adapthist(im, kernel_size=kern_size[j], clip_limit=clip_lim[j]) # adaptive equalization
-            plt.subplot(2,length,j+1)
-            plt.imshow(adapt_hist_eq_image, cmap='gray')
-            plt.title("K: " + str(kern_size[j]) + " C: " + str(clip_lim[j]))
-            plt.subplot(2,length,length + j + 1)
-            adapt_hist, bin_edges = np.histogram(eq_image,bins) # get the histogram
-            plt.bar(np.arange(0, 256, 256/bins), adapt_hist) # show the histogram of the equalized image
-            plt.title("Adapt. Hist.")
-
-        for curr_ax in ax.flatten():
-            curr_ax.set_box_aspect(1.0) # set the plots to be square
-            curr_ax.ticklabel_format(style='sci', axis='y', scilimits=(0,2)) # format x axis to display in scientific notation
-        plt.savefig("output" + os.path.sep + files[i].split('.')[0] + "_ah.png")
-        plt.close('all')
-
+        im = io.imread(dir_path + img_path + img_filenames[i], as_gray=True) # import and convert image to gray scale
+        ocr(image=im)
         print() # add an extra space between images
     plt.show()  # make sure matplotlib shows the plot created above
     return
 
 if __name__ == "__main__":
     main()
-
-
-# def floodFill(px_x, px_y, im, threshold):
-#     # neighborhood can be modified to include any pixel offsets desired
-#     neighborhood = [(-1, 0), # left
-#                     ( 0, 1), # up
-#                     ( 1, 0), # right
-#                     ( 0,-1)] # down
-#     width = im.shape[1]
-#     height = im.shape[0]
-#     out_im = np.int(np.zeros_like(im))
-
-#     # check if out of bounds or if the current pixel is false
-#     if (px_x < 0 or px_x > width
-#         or px_y < 0 or px_y > height
-#         or ~threshold[px_y, px_x]): 
-#         return out_im
-
-#     # create new set of pixel locations to check (sets contain only unique values)
-#     px_set = {(px_x, px_y)}
-#     while(len(px_set) > 0):
-#         (px_x, px_y) = px_set.pop()
-#         for n in neighborhood: # loop through all the pixels in the current neighborhood
-#             test_x = px_x + n[0]
-#             test_y = px_y + n[1]
-#             if not (test_x < 0 or test_x > width - 1 or test_y < 0 or test_y > height - 1): # check if out of bounds
-#                 if (threshold[test_y, test_x] and out_im[test_y, test_x] == 0): # if it passes the test function and is not already marked as a connected component
-#                     px_set.add((test_x,test_y)) # add the location to the pixel location set to later check its neighbors
-#                     out_im[test_y, test_x] = 1  # mark that pixel as being part of a connected set of pixels   
-#     return out_im
-
-# def connectedComponents(im, threshold):
-#     cc = np.int32(np.zeros_like(im))
-#     component = 1
-#     px_to_check = np.fliplr(np.transpose(np.nonzero(threshold))) # https://stackoverflow.com/a/21815619
-#     for i in np.arange(np.size(px_to_check,0)):
-#         curr_px = px_to_check[i]
-#         if (cc[curr_px[1], curr_px[0]] == 0): # if there isn't already a connected component at that coordinate, check it
-#             ff = floodFill(curr_px[0], curr_px[1], im, threshold) # make returned set have correct label number
-#             if (np.sum(ff) > 200):
-#                 cc += ff * component       # add the new connected component to the output buffer cc
-#                 component += 1  # increment component number
-#     return cc
