d0d58434b791d4ea6c4a6981845fe7b2dbc74f03
Modarius <alancfelt@gmail.com>
2022-12-03 00:04:22 -0700

    pre-change to reduce dataframe usage

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from copy import deepcopy
 import os
 import numpy as np
 import pandas as pd
+import time
 import matplotlib.pyplot as plt
 import skimage.io as io
 import skimage.measure as measure
@@ -17,10 +18,10 @@ import skimage.restoration as restore
 import skimage.transform as transform
 import predict as predict
 
-def connectedComponents(image, min_size=5):
+def connectedComponents(image):
     out_im = np.zeros_like(image,dtype=int)
-    image = morph.remove_small_objects(image, min_size=min_size)
-    image = morph.remove_small_holes(image, area_threshold=min_size)
+    image = morph.remove_small_objects(image, min_size=4)
+    image = morph.remove_small_holes(image, area_threshold=4)
     [labeled_im, num] = morph.label(image, return_num=True, connectivity=2)
     if (num > 1): # prevents case where only one label is passed to remove_small_objects() which throws a warning
         labels = np.unique(labeled_im) # find all the labels in the image
@@ -36,8 +37,8 @@ def connectedComponents(image, min_size=5):
 
 def ocr(image):
     predictor = predict.CharacterPredictor(model_path="model.pth")
-    denoised_im = restore.denoise_bilateral(image)
-    threshold_im = np.round(restore.denoise_nl_means(denoised_im < filters.threshold_otsu(denoised_im))).astype(bool)
+    denoised_im = restore.denoise_nl_means(image)
+    threshold_im = np.round(denoised_im < filters.threshold_otsu(denoised_im)).astype(bool)
     labeled_im = connectedComponents(threshold_im)
     props = ["label", "bbox", "image_intensity"]
     im_props = pd.DataFrame(measure.regionprops_table(labeled_im, intensity_image=(labeled_im > 0).astype(np.int8), properties=props))
@@ -49,37 +50,28 @@ def ocr(image):
     while(process_props.__len__() > 0):
         chars.append([])
         idx_tl = process_props['sum_x_y'].idxmin() # index of the top left point
-        tl_props = dict(process_props.loc[idx_tl])
-        current_row = process_props.query("min_row <" + str(tl_props['max_row'])) # get the characters in the current row
+        current_row = process_props.query("min_row <" + str(process_props.loc[idx_tl]['max_row'])).copy(deep=True) # get the characters in the current row
         process_props.drop(index=current_row.index,inplace=True) # remove them from the list of all properties
         current_row.sort_values(by="min_col", inplace=True) # order left to right
         i=0
-        figure = plt.figure()
         for idx in current_row.index:
             i += 1
             curr_img = current_row.loc[idx]['image_intensity']
-            scaled_img = transform.rescale(curr_img, 28 / np.array(curr_img.shape).max(), anti_aliasing=False)
-            print(scaled_img.shape)
+            scaled_img = transform.rescale(curr_img, 26 / np.array(curr_img.shape).max(), anti_aliasing=False)
             pad_x_begin = 0
             pad_x_end = 0
             pad_y_begin = 0
             pad_y_end = 0
             if(scaled_img.shape != (28, 28)):
                 row, col = scaled_img.shape
-                pad_x_begin = np.floor((28 - col)/2).astype(np.int8)
-                pad_x_end = np.ceil((28 - col)/2).astype(np.int8)
-                pad_y_begin = np.floor((28-row)/2).astype(np.int8)
-                pad_y_end = np.ceil((28-row)/2).astype(np.int8)
+                pad_x_begin = int(np.floor((28 - col)/2))
+                pad_x_end = int(np.ceil((28 - col)/2))
+                pad_y_begin = int(np.floor((28-row)/2))
+                pad_y_end = int(np.ceil((28-row)/2))
             padded_img = np.pad(scaled_img, pad_width=((pad_y_begin, pad_y_end),(pad_x_begin, pad_x_end)))
             char_img = (padded_img > 0).astype(np.int8)
-            ax1 = plt.subplot(2, len(current_row), i)
-            ax2 = plt.subplot(2, len(current_row), i + len(current_row))
-            ax1.imshow(char_img)
-            ax2.imshow(curr_img)
-            plt.setp(figure.axes, xticks=[], yticks=[])
             chars[-1].append(predictor.predict(char_img))
-        plt.close('all')
-    return
+    return chars
 
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__)) + os.sep # https://stackoverflow.com/a/5137509
